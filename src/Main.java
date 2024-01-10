16c19400f39462350dde6d7393c52b9c71861c67
Joaquim Magode <joaquimcesarmagode@gmail.com>
2024-01-10 16:10:32 +0530

    FileName_Update

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,30 @@ import java.io.FileWriter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    File myObj = new File("filename.txt");
+    if(myObj.exists()){
+        System.out.println("File Name: "+myObj.getName());
+        System.out.println("Path: "+myObj.getAbsolutePath());
+        System.out.println("Writteble: "+myObj.canWrite());
+        System.out.println("Readable: "+myObj.canRead());
+        System.out.println("File size in bytes: "+myObj.length());
+    }
+
+      /*
+        File myObj = new File("filename.pdf");
         try {
-            FileWriter myObj = new FileWriter("fileNamed.txt");
-            myObj.write("Hello Guys I'm Writting now");
-            myObj.close();
-        }catch (Exception e)
-        {
+            if (myObj.createNewFile()){
+                System.out.println("File Successfully Created");
+            }else
+            {
+                System.out.println("File Already Exists");
+                if (myObj.exists()){
+                }
+            }
+        }catch (Exception e){
             System.out.println(e.getMessage());
         }
-
+*/
     }
 }
