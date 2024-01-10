05491bf8ce750d5b8d0ab8afb09a1e3a0c5d6e8b
Joaquim Magode <joaquimcesarmagode@gmail.com>
2024-01-10 15:57:41 +0530

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 import java.io.File;
+import java.io.FileWriter;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        File myObj = new File("filename.txt");
         try {
-            if (myObj.createNewFile()){
-                System.out.println("File Successfully Created");
-            }else
-            {
-                System.out.println("File Already Exists");
-            }
-        }catch (Exception e){
+            FileWriter myObj = new FileWriter("fileNamed.txt");
+            myObj.write("Hello Guys I'm Writting now");
+            myObj.close();
+        }catch (Exception e)
+        {
             System.out.println(e.getMessage());
         }
 
