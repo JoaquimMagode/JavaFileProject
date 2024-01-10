3003ab709e67a3adb632adfa2d8ee5bb587203f6
Joaquim Magode <joaquimcesarmagode@gmail.com>
2024-01-10 15:41:44 +0530

    Update
    File Create

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         File myObj = new File("filename.txt");
+        try {
+            if (myObj.createNewFile()){
+                System.out.println("File Successfully Created");
+            }else
+            {
+                System.out.println("File Already Exists");
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
